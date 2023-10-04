3de121fc2dedf6bac3dd2f337e65903cb5e954f9
Raafat Turki <raafat.turki@proton.me>
2023-10-04 18:53:52 +0300

    feat: add conditions page popover toggles

--- src/components/ConditionCard.jsx
+++ src/components/ConditionCard.jsx
@@ -1,18 +1,19 @@
-import { useState } from "react";
+import {Popover, PopoverTrigger, PopoverContent, Button} from "@nextui-org/react";
 
 function ConditionCard({ condition }) {
-    const [cardState, setCardState] = useState("hidden");
-   
-
-    function onClick(){
-        setCardState(cardState === "hidden" ? "block bg-orange-100 m-2 h-full font-['Modesto expanded']" : "hidden");
-    }
-
   return (
-      <button className="condition-card flex flex-wrap justify-around content-center border-double border-4 border-orange-800 h-[20rem] overflow-y-scroll " onClick={onClick}>
-        <h1 className="font-['Modesto Condesed']">{condition.name}</h1>
-        <p className={cardState}>{condition.desc}</p>
-      </button>
+    <>
+      <Popover placement="left">
+        <PopoverTrigger>
+          <h1 className="font-['Modesto Condesed']">{condition.name}</h1>
+        </PopoverTrigger>
+        <PopoverContent className="text-base">
+          <div className="w-64">
+            <p>{condition.desc}</p>
+          </div>
+        </PopoverContent>
+      </Popover>
+    </>
   );
 }
 
--- src/screentabs/Conditions.jsx
+++ src/screentabs/Conditions.jsx
@@ -3,12 +3,13 @@ import { DnDContext } from '../store/store';
 import ConditionCard from '../components/ConditionCard';
 
 function Conditions() {
-  
-const {conditions} = useContext(DnDContext);
-  return <div className='conditions grid mx-1 gap-4 grid-cols-3 grid-rows-3'>
-    {conditions.map((condition)=>
-    <ConditionCard condition={condition}/>)}
-    </div>;
+
+  const {conditions} = useContext(DnDContext);
+  return (
+    <div className='h-full grid grid-cols-3'>
+      { conditions.map((condition) => <ConditionCard condition={condition}/>) }
+    </div>
+  )
 }
 
 export default Conditions;
